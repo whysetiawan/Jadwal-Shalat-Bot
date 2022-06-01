9870795cd80248b8ec70989f8b8ad2249047f6f6
wahyu.setiawan <wahyu.setiawan@sicepat.com>
2022-06-01 18:00:46 +0700

    change token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { PrayScheduleModule } from 'src/pray-schedule/pray-schedule.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TelegrafModule.forRoot({
-      token: '5471483054:AAF_KsBW49tFYa-TJ82NyRTB9ktgf4AWj0A',
+      token: process.env.TELEGRAM_TOKEN,
       include: [PrayScheduleModule],
     }),
     PrayScheduleModule,
