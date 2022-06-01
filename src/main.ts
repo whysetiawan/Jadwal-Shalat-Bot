54dbffd907c955fb8399530555c2bc90c5fe7207
wahyu.setiawan <wahyu.setiawan@sicepat.com>
2022-06-01 18:20:12 +0700

    feat:change nest application context to normal express

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  await NestFactory.createApplicationContext(AppModule);
-  // await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
